03e9aeda32c0053d47fd6f949f079cd82ca31dfd
code-rmc <rodrigoslmatias@gmail.com>
2022-03-18 11:52:00 -0300

    add remove mark, fix styled & index

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 const mymap = L.map("mapid").setView([-34.6083, -58.3712], 5);
 
+// Vista del mapa al cargar la pagina
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  maxZoom: 18,
+  tileSize: 512,
+  zoomOffset: -1,
+}).addTo(mymap);
+
+// Agregar Ubicaciones desde JSON
+/***************** */
+const url = "./data/locations.json";
+
+const locationPoints = (location) => {
+  const groupLocation = location.map((mark) => {
+    const [long, lat] = mark.coord.split(",");
+    return L.marker([long, lat]).bindPopup(`
+        <b>"Descripcíon"</b>: ${mark.descripcion},
+        <br><b>"Dirección"</b>: ${mark.direccion},
+        <br><b>"Telefónico"</b>: ${mark.telefono},
+        <br><b>"coord"</b>: ${mark.coord},
+        <br><b>"Categoría"</b>: ${mark.categoria},
+    `);
+  });
+
+  L.layerGroup(groupLocation).addTo(mymap);
+};
+
+const importLocation = () =>
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => locationPoints(data));
+
+importLocation();
+/********************************** */
+
 const camps = ["description", "address", "phone", "category", "coord"];
-// Ubicaciones agregadas
-const locations = [];
 
 const form = document.querySelector(".formMarker");
 const searchForm = document.getElementById("searchForm");
 const alertValid = document.getElementById("alertValidation");
 const coord = document.getElementById("floatingCoord");
+const removeButton = document.querySelector(".removeButton");
+let marcadorRemove; // guardar el marcador a eliminar
 
 // Validacion de Coordenadas
 const validationCoords = (long, lat) => {
@@ -28,6 +66,13 @@ const validationCoords = (long, lat) => {
   return true;
 };
 
+// Eliminar marcador
+/***************** */
+function removePoint() {
+  mymap.removeLayer(marcadorRemove);
+  removeButton.style.display = "none";
+}
+
 // Formulario de busqueda
 /***************** */
 searchForm.addEventListener("submit", (e) => {
@@ -41,82 +86,73 @@ searchForm.addEventListener("submit", (e) => {
   mymap.flyTo([long, lat], 18);
 });
 
-// Formulario
+// Formulario agregar marcador
 /**************** */
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  mymap.closePopup(); // Cerrar popup
   alertValid.style.display = "none";
 
   const dataForm = new FormData(form);
   const fields = camps.map((field) => dataForm.get(field));
 
   const [long, lat] = fields[4].split(",");
-  if (validationCoords(long, lat)) {
-    return;
-  }
 
-  L.marker([long, lat])
+  if (validationCoords(long, lat)) return;
+
+  const mark = L.marker([long, lat]);
+
+  mark
     .bindPopup(
       `
       <b>Descripción</b>: ${fields[0]}
       <br><b>Dirección</b>: ${fields[1]}
       <br><b>Telefónico</b>: ${fields[2]}
       <br><b>(X , Y)</b>: ${fields[4]}
-      <br><b>Categoría</b>: ${fields[3]}.`
+      <br><b>Categoría</b>: ${fields[3]}
+      `
     )
     .addTo(mymap);
+
+  mark.on("popupopen", (e) => {
+    marcadorRemove = e.target;
+    removeButton.style.display = "inline-block";
+  });
+
+  form.reset();
 });
 
-// Punto de primer vista del mapa al cargar la pagina
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  maxZoom: 18,
-  tileSize: 512,
-  zoomOffset: -1,
-}).addTo(mymap);
+// const circle = L.circle([51.508, -0.11], {
+//   color: "red",
+//   fillColor: "#f03",
+//   fillOpacity: 0.5,
+//   radius: 500,
+// }).addTo(mymap);
 
-const littleton = L.marker([39.61, -105.02]).bindPopup(
-    "This is Littleton, CO."
-  ),
-  denver = L.marker([39.74, -104.99]).bindPopup("This is Denver, CO."),
-  aurora = L.marker([39.73, -104.8]).bindPopup("This is Aurora, CO."),
-  golden = L.marker([39.77, -105.23]).bindPopup("This is Golden, CO.");
+// circle.bindPopup("I am a circle.");
 
-const cities = L.layerGroup([littleton, denver, aurora, golden]).addTo(mymap);
+// const marker = L.marker([-34.595986, -58.3724715]).addTo(mymap);
 
-const circle = L.circle([51.508, -0.11], {
-  color: "red",
-  fillColor: "#f03",
-  fillOpacity: 0.5,
-  radius: 500,
-}).addTo(mymap);
-
-circle.bindPopup("I am a circle.");
-
-const marker = L.marker([-34.595986, -58.3724715]).addTo(mymap);
-
-marker
-  .bindPopup(
-    `<b>Descripción</b>: AEROTERRA S.A.
-      <br><b>Dirección</b>: Av. Eduardo Madero 1020
-      <br><b>Telefónico</b>: 54 9 11 5272 0900
-      <br><b>(X , Y)</b>: -34.595986,-58.3724715
-      <br><b>Categoría</b>: Residencial.`
-  )
-  .openPopup();
+// marker
+//   .bindPopup(
+//     `<b>Descripción</b>: AEROTERRA S.A.
+//       <br><b>Dirección</b>: Av. Eduardo Madero 1020
+//       <br><b>Telefónico</b>: 54 9 11 5272 0900
+//       <br><b>(X , Y)</b>: -34.595986,-58.3724715
+//       <br><b>Categoría</b>: Residencial.`
+//   )
+//   .openPopup();
 
 /********************************* */
 // Funcion para hacer click en el mapa
-const popup = L.popup();
-
 const onMapClick = (e) => {
+  removeButton.style.display = "none";
   const { lat, lng } = e.latlng;
 
-  popup
+  L.popup()
     .setLatLng(e.latlng)
     .setContent(
-      `Coordenada copiada <br>latitud: ${lat
+      `Coordenada copiada!!! <br>latitud: ${lat
         .toString()
         .slice(0, 8)} <br>logitud: ${lng.toString().slice(0, 8)}`
     )
